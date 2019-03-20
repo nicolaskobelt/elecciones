d877c848d2495ff0bffb60b8eeaf715d75f54633
nicolaskobelt <kobeltnicolas@me.com>
2019-03-20 18:33:31 -0300

    V2.2

--- server.js
+++ server.js
@@ -9,4 +9,3 @@ app.use('/', serveStatic(path.join(__dirname, '/dist')))
 const port = process.env.PORT || 8080
 app.listen(port)
 
-console.log('listening on port: ' + port )
--- src/components/Contacto.vue
+++ src/components/Contacto.vue
@@ -1,32 +1,5 @@
 <template>
-    <v-card
-            flat
-    >
-        <v-container
-                fluid
-                grid-list-lg
-                hover
-        >
-            <v-layout row wrap>
-                <v-flex xs12>
-                    <v-card color="green darken-1" class="white--text">
-                        <v-layout row wrap class="mb-1">
-                            <v-flex xs7>
-                                <v-card-title primary-title>
-                                    <div>
-                                        <div class="headline">WhatsApp</div>
-                                        <div></div>
-                                    </div>
-                                </v-card-title>
-                            </v-flex>
-                            <v-flex xs5>
-
-                            </v-flex>
-                        </v-layout>
-                    </v-card>
-                </v-flex>
-
-            </v-layout>
-        </v-container>
-    </v-card>
+    <div>
+        <p>contacto page</p>
+    </div>
 </template>
