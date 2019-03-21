ae14a499e2db57a144fdb4aebae32045325d3827
nicolaskobelt <kobeltnicolas@me.com>
2019-03-21 10:11:24 -0300

    V2.3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const serveStatic = require("serve-static")
 const path = require('path')
 
-App = express();
-App.use(serveStatic(path.join(__dirname, 'dist')));
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
-App.listen(port);
+app.listen(port);
