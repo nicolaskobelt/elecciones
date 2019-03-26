c2aa473fdd89a9f115d7b873ab4cfc2463a73fe8
nicolaskobelt <kobeltnicolas@me.com>
2019-03-26 11:36:20 -0300

    atrr

--- server.js
+++ server.js
@@ -5,6 +5,4 @@ const path = require('path')
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 // const port = ;
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 3000);
