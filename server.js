735d1aaf6fd2d4d3a47de57902b61a2d18d6fe7c
nicolaskobelt <kobeltnicolas@me.com>
2019-03-26 11:19:44 -0300

    pizza con pala

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const path = require('path')
 
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+// const port = ;
+app.listen(process.env.PORT || 5000);
