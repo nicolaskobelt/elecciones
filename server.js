ebc656244d6bade35bc28bd9e8efa6d4ba94108f
nicolaskobelt <kobeltnicolas@me.com>
2019-03-28 15:05:09 -0300

    V2.5.1

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ const path = require('path')
 
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use('/', express.static('public/public'));
 // const port = ;
 app.listen(process.env.PORT || 3000);
