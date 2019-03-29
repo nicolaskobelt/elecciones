7f337dd7432ac2e40fe7258f48043f4de8921e30
nicolaskobelt <kobeltnicolas@me.com>
2019-03-29 00:37:12 -0300

    V2.6

--- src/App.vue
+++ src/App.vue
@@ -59,8 +59,8 @@ export default {
         menuItem: [
             {icon: 'bar_chart', title:'Resultados generales', link: '/'},
             {icon: 'show_chart', title:'Resultados por mesa', link: '/pormesa'},
-            {icon: 'mail', title: 'Contacto', link: '/contacto'}
-
+            {icon: 'mail', title: 'Contacto', link: '/contacto'},
+            {icon: 'surround_sound', title: 'Radio Online'}
         ],
     }
   },
--- src/components/Contacto.vue
+++ src/components/Contacto.vue
@@ -1,5 +1,73 @@
 <template>
-    <div>
-        <p>contacto page</p>
-    </div>
+    <v-card>
+        <v-container
+                fluid
+                grid-list-lg
+            >
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-card color="green darken-3" class="white--text">
+                    <v-card-title primary-title>
+                        <div>
+                            <div class="headline">Whatsapp</div>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn
+                                flat dark
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                href="https://wa.me/5493546450736"
+                        >
+                                Enviar Mensaje!</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12>
+                <v-card color="blue darken-4" class="white--text">
+                    <v-card-title primary-title>
+                        <div>
+
+                            <div class="headline">Facebook</div>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn
+                                flat dark
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                href="https://m.me/lasenalfm"
+                        >
+                            Enviar Mensaje!</v-btn>
+                        <v-btn
+                            flat dark
+                            href="https://www.facebook.com/lasenalfm/"
+                        > Ir a la Fan Page</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12>
+                <v-card color="purple darken-2" class="white--text">
+                    <v-card-title primary-title>
+                        <div>
+                            <div class="headline">Email</div>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn
+                                flat dark
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                href="mailto:info@fm977.com.ar"
+                        >
+                            Enviar Mail!</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+
+        </v-layout>
+            </v-container>
+    </v-card>
 </template>
